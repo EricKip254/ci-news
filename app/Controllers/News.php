89e1cfdff823d39188cf141d85195ace263e5ce2
EricKip254 <bantakip@yahoo.com>
2023-06-02 12:30:55 +0300

    Added View News Item

--- app/Controllers/News.php
+++ app/Controllers/News.php
@@ -1,8 +1,8 @@
 <?php 
 namespace App\Controllers;
 
-use CodeIgniter\CodeIgniter;
 use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 
 class News extends BaseController{
@@ -28,7 +28,20 @@ class News extends BaseController{
         // $model = new NewsModel(); used when not in use
         $model = model(NewsModel::class);
 
+
         $data['news'] = $model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view('templates/header', $data)
+            . view('news/view')
+            . view('templates/footer');
     }
+
+   
 }
 ?>
